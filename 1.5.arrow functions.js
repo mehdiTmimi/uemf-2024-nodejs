3410588e866730dde0f219ad3d69c07b5cdb288d
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2024-09-27 14:26:40 +0100

    ok

---
+++ 1.5.arrow functions.js
@@ -0,0 +1,35 @@
+// arrow function sont des fonctions anonyme
+// contrairement aux fonction (function) ils ne permettent pas de creer des classes
+// syntax :
+// (params) => { contenu }
+// exemple: (a,b)=>{ 
+//    let s = a+b; 
+//    return s;
+//}
+// =====> abreviations
+// 1 - les () sont facultatifs si on a 1 seul param
+// 2- les {} sont facultatifs sin on a 1 seule instruction
+// 3- dans l'absence des {} => un return implicite est rajoute
+
+// a=> a>0 
+// function(a){return a>0} => il determine est ce que le nombre est positif ou negatif
+
+
+/*function x(){
+    console.log("salam");
+}
+ let x = ()=> console.log("salam"); 
+*/
+
+/*
+let F=()=>{let y=10;
+    console.log(y)
+}
+
+let F = function(){
+    let y=10;
+    console.log(y)
+}
+ */
+
+
