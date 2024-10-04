0ec9e13a7efdd875c95c5089f1cf4828a8fb2238
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2024-10-04 11:11:56 +0100

    ok

---
+++ 1.9.json.js
@@ -0,0 +1,9 @@
+// JSON = Javascript Object Notation
+// format de representation des donnees
+// syntax:
+// clef:valeur
+// clef de la properiete/attribut est ecrite entre ""
+// valeur : number, String (""), boolean, object ({}), array([])
+
+// du JSON Text vers Objet => JSON.parse(chaine JSON)
+// du Objet vers JSON => JSON.stringify(Objet js)
