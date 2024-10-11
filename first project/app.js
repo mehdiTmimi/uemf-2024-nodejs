839647ceda65da51731be80975e684b5861d019c
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2024-10-11 09:39:25 +0100

    ok

--- first project/app.js
+++ first project/app.js
@@ -1,13 +1,16 @@
 const fs = require("fs") //
 
+// en mode asynchrone
 fs.promises.writeFile("./data.txt","salut uemf").then(()=>{
     console.log("success")
 }).catch(e=>console.error(e))
+// en mode asynchrone
 fs.writeFile("./data2.txt","salut uemf",(err)=>{
     if(err)
         return console.error(err)
     console.log("success2")
 })
+// en mode synchrone
 try{
 fs.writeFileSync("./data3.txt","salut uemf")
 console.log("success3")
@@ -15,3 +18,6 @@ console.log("success3")
 {
     console.error(e)
 }
+// difference:
+// en mode synchrone, on peut passer a la prochaine instruction sans
+// terminer l instruction actuelle
