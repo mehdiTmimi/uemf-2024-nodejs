f8325045c880791747b60388497ce507d8e086db
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2024-10-18 08:38:00 +0100

    ok

--- 1.10.promises-suite.js
+++ 1.10.promises-suite.js
@@ -1,14 +1,22 @@
 const fs = require("fs")
 // callback (async)
-fs.readFile("./data.json",(err,data)=>{
+fs.readFile("./data.json", (err, data) => {
     let todos = JSON.parse(data)
     console.log(todos.length)
 })
 // promise => cette maniere est async
-fs.promises.readFile("./data.json").then((data)=>{
+fs.promises.readFile("./data.json").then((data) => {
     let todos = JSON.parse(data)
     console.log(todos.length)
-}).catch((err)=>console.log(err))
+}).catch((err) => console.log(err))
+// mode sync
+try {
+    let data = await fs.promises.readFile("./data.json")
+    let todos = JSON.parse(data)
+    console.log(todos.length)
+}catch(err){
+    console.log(err)
+}
 
 // classic (sync)
 let data = fs.readFileSync("./data.json")
---
+++ callback_hell.js
@@ -0,0 +1,40 @@
+let f1 = (nbr, successFn, echecFn) => {
+    setTimeout(() => successFn(nbr + 1), 2000)
+}
+let f2 = (nbr, successFn, echecFn) => {
+    setTimeout(() => successFn(nbr * 2), 3000)
+}
+let f3 = (nbr, successFn, echecFn) => {
+    setTimeout(() => successFn(nbr - 1), 4000)
+}
+let main = () => {
+    let x = 5
+    f1(x, (x2) => {
+        f2(x2, (x3) => {
+            f3(x3, (x4) => { console.log(x4) }, () => { })
+        }, () => { })
+    }, () => { })
+}
+//main()
+let ff1 = (nbr) => new Promise((resolve, reject) => {
+    //reject("error ff1")
+    setTimeout(() => resolve(nbr + 1), 2000)
+})
+let ff2 = (nbr) => new Promise((resolve, reject) => {
+    reject("error ff2")
+    setTimeout(() => resolve(nbr * 2), 3000)
+})
+let ff3 = (nbr) => new Promise((resolve, reject) => {
+    setTimeout(() => resolve(nbr - 1), 4000)
+})
+let main2 = () => {
+    let x = 5
+    ff1(x)
+    .then(data=>{return ff2(data);},
+    e=>console.log("ali",e))
+    .then(data=>ff3(data))
+    .then(data=>console.log(data))
+    .catch(e=>console.log("mehdi",e))
+}
+main2()
+
