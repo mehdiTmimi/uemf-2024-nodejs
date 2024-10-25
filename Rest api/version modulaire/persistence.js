25975467fb319d8b08cb2f2de6a11fa9b9a91690
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2024-10-25 12:13:16 +0100

    ok

--- Rest api/version modulaire/persistence.js
+++ Rest api/version modulaire/persistence.js
@@ -14,8 +14,31 @@ const search2 = (id) => new Promise((resolve, reject) => {
         .then(data => JSON.parse(data))
         .then(data => { // c'est l'objet users du database.json
             let resultat = data.users.find(value => value.id == id)
-            resolve( resultat == undefined ? null : resultat)
+            resolve(resultat == undefined ? null : resultat)
         })
-        .catch(e=>reject(e))
+        .catch(e => reject(e))
 });
-search2(1).then(data => console.log(data))
+
+const getAll = async () => {
+    let data = await fs.promises.readFile(PathDb)
+    data = JSON.parse(data.toString())
+    return data
+}
+
+const removeUser = async (id) => {
+    let user = await search2(id)
+    if (!user)
+        return null
+    let { users } = await getAll()
+    /*
+    let data = await getAll()
+    let users = data.users
+    */
+    users = users.filter(val => val.id != id)
+    await fs.promises.writeFile(PathDb, JSON.stringify({ users }, null, 3))
+    return user
+}
+removeUser(1).then(res=>console.log(res))
+module.exports = {
+    search,search2,getAll,removeUser
+}
