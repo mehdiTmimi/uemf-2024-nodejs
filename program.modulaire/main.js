9c6d2aae390dc540b0e123c46580424672c51876
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2024-10-25 10:57:08 +0100

    ok

---
+++ program.modulaire/main.js
@@ -0,0 +1,4 @@
+let {somme,admin} = require("./utils.js")
+// object destructing
+console.log(somme(1,5))
+console.log(admin)
---
+++ program.modulaire/utils.js
@@ -0,0 +1,12 @@
+console.log("debut utils")
+const PI = 22 / 7
+const PORT = 3000
+const somme = (a, b) => a + b
+const admin = { nom: "ali", age: 45 }
+console.log("fin utils")
+module.exports ={
+    PI : PI,
+    p : PORT,
+    somme,
+    admin
+}
