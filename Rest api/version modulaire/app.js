f2067ab91b869d33d8a7c34f9de0b9ccd6a6c939
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2024-10-25 12:16:14 +0100

    ok

--- Rest api/version modulaire/app.js
+++ Rest api/version modulaire/app.js
@@ -4,3 +4,9 @@
 
 // persistence.js => gestion de la base de donnes
 // => search, delete , getAll
+const http = require("http")
+// le callback du createServer vous allez l importer du routes.js
+const server = http.createServer()
+server.listen(3000,()=>{
+    console.log("server started at 3000")
+})
