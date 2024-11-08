0ba80b8f07de7abb69fa2de280f655f0f6dc8dd1
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2024-11-08 11:14:29 +0100

    ok

--- send-data-request/app.js
+++ send-data-request/app.js
@@ -13,34 +13,16 @@ const server = http.createServer((req, res) => {
     // /add2?task=cook&duration=3h
     if(method=="POST" && url.startsWith("/add2?")){
         let params = url.split("?")[1] 
-        params = new URLSearchParams(params)
-        
+        params = new URLSearchParams(params)   
         let task = params.get("task")
         let duration = params.get("duration")
-        console.log(task,duration)
-        
+        return sendResponse(task,duration,res)
     }
     // add1 en utilisant les headers
     else if (url == "/add1" && method == "POST") {
         console.log("/add1 context (headers)")
         let { task, duration } = req.headers
-        if (!task || !duration) {
-            // status code 400
-            res.statusCode = 400
-            res.write("please fill all the required fields")
-            return res.end()
-        }
-        saveTodo({ task, duration }).then(()=>{
-            res.statusCode=201
-            res.write("insertion reussie")
-            res.end()
-        })
-            .catch((e) => {
-                console.log(e)
-                res.statusCode = 500
-                res.write("server problem ! sorry")
-                res.end()
-            })
+        return sendResponse(task,duration,res)
     }
     else {
         res.statusCode = 404
@@ -48,4 +30,24 @@ const server = http.createServer((req, res) => {
         res.end()
     }
 })
+
+const sendResponse = (task,duration,res)=>{
+    if (!task || !duration) {
+        // status code 400
+        res.statusCode = 400
+        res.write("please fill all the required fields")
+        return res.end()
+    }
+    saveTodo({ task, duration }).then(()=>{
+        res.statusCode=201
+        res.write("insertion reussie")
+        res.end()
+    })
+        .catch((e) => {
+            console.log(e)
+            res.statusCode = 500
+            res.write("server problem ! sorry")
+            res.end()
+        })
+}
 server.listen(PORT, () => console.log(`server started at ${PORT}`))
